3c7eccb94eca3a8915762b278c91132b1216efa2
Sachinreddy3028 <65082681+Sachinreddy3028@users.noreply.github.com>
2025-02-13 13:48:56 +0000

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -16,10 +16,10 @@ s3_client = boto3.client('s3')
 @app.route("/upload", methods=["POST"])
 def upload_file():
     file = request.files.get("file")
-if file:
-    s3_client.upload_fileobj(file, S3_BUCKET, file.filename,ExtraArgs={"ServerSideEncryption": "AES256"})
-            return {"message": "File uploaded securely."}, 200
-return {"error": "No file provided."}, 400
+    if file:
+        s3_client.upload_fileobj(file, S3_BUCKET, file.filename,ExtraArgs={"ServerSideEncryption": "AES256"})
+        return {"message": "File uploaded securely."}, 200
+     return {"error": "No file provided."}, 400
 
-if _name_ == "_main_":
-    app.run(host="0.0.0.0", port=5000)
+ if _name_ == "_main_":
+     app.run(host="0.0.0.0", port=5000)
